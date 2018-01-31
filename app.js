2cb086bb98200f5b27f1832bd195fbf242cd1c9b
Cameron Teasdale <cameronteasdale@Camerons-MacBook-Pro.local>
2018-01-30 21:18:16 -0500

    added email, fixed given time

--- app.js
+++ app.js
@@ -10,12 +10,16 @@ app.use(express.static(__dirname + "views"))
 app.listen(process.env.PORT,process.env.IP, function(){
   console.log("server started")
 })
+// app.listen(3000, function(){
+//   console.log("server started")
+// })
 
 var setTimeoutPromise = util.promisify(setTimeout);
 
 
 var startedTime = Date.now();
 var wait;
+var startTime;
 console.log(startedTime)
 getRandomTime();
 function getRandomTime() {
@@ -24,33 +28,35 @@ function getRandomTime() {
   while (moment(Date.now() + wait).toObject().hours > 21 || moment(Date.now() + wait).toObject().hours < 6){
     wait = Math.round(Math.random()*86400*1000);
   }
-  console.log(moment(Date.now() + wait).format('LT'));
+  startTime = Date.now();
+  console.log(moment(startTime + wait).format('LT'));
   setTimeoutPromise(wait).then((value) => {
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
     getRandomTime();
   });
 }
 var transporter = mail.createTransport({
   service: 'gmail',
   auth: {
-    user: 'sharynsmith143@gmail.com',
+    user: 'dailyphoto8000@gmail.com',
     pass: 'waper143'
   }
 });
 
 var mailOptions = {
-  from: 'sharynsmith143@gmail.com',
+  from: 'dailyphoto8000@gmail.com',
   to: 'sharynsmith143@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+  subject: 'Take a picture',
+  text: ':)'
 };
 
 app.get("/", function(req,res){
-  // transporter.sendMail(mailOptions, function(error, info){
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     console.log('Email sent: ' + info.response);
-  //   }
-  // });
-  res.render("index", {time:moment(Date.now() + wait).format('LT')});
+
+  res.render("index", {time:moment(startTime + wait).format('LT')});
 })
