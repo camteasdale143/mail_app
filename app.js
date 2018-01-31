d5c7951d5e9942ac8eb416ae9d5ff97e1a08af92
Cameron Teasdale <cameronteasdale@Camerons-MacBook-Pro.local>
2018-01-31 07:21:05 -0500

    chang listening for upload to server

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "views"))
-// app.listen(process.env.PORT,process.env.IP, function(){
-//   console.log("server started")
-// })
-app.listen(3000, function(){
+app.listen(process.env.PORT,process.env.IP, function(){
   console.log("server started")
 })
+// app.listen(3000, function(){
+//   console.log("server started")
+// })
 
 
 var startedTime = Date.now();
