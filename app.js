43f9ca71653fa75ed7b33cd42e0a3815f390f4a9
Cameron Teasdale <cameronteasdale@Camerons-MacBook-Pro.local>
2018-01-30 21:01:15 -0500

    add process.env.port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "views"))
-app.listen(3000, function(){
+app.listen(process.env.PORT,process.env.IP, function(){
   console.log("server started")
 })
 
